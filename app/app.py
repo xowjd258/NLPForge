a25836d3a2b25626ec91c77ff907f75783fabbee
xowjd258 <xowjd2585@naver.com>
2024-04-02 23:08:41 +0900

    add: models.py embedding model,add: app.py, models.py method comment

--- app/app.py
+++ app/app.py
@@ -18,18 +18,20 @@ from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 
 
-app = FastAPI(   title="NLPForge",
+app = FastAPI(
+    title="NLPForge",
     version="0.0.1",
-    description="An all-in-one NLP suite for enhancing language tasks, from data analysis to chatbot creation.",
+    description="데이터 분석부터 챗봇 생성까지 언어 작업을 향상시키기 위한 NLP 통합 솔루션을 제공합니다.",
     contact={
-    "name": "T.J. KIM",
-    "email": "xowjd258@hanyang.ac.kr",
+        "name": "T.J. KIM",
+        "email": "xowjd258@hanyang.ac.kr",
     },
     license_info={
-    "name": "Apache 2.0",
-    "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
-    openapi_url="/api/v1/openapi.json")
+    openapi_url="/api/v1/openapi.json"
+)
 # 모델 정의
 app.add_middleware(
     CORSMiddleware,
@@ -48,18 +50,46 @@ chatbot_router = APIRouter(prefix="/chatbot", tags=["Chatbot"])
 # 라우터에 엔드포인트 추가
 @waybill_router.post("/classify")
 async def classify_waybill(item: WaybillItem):
+    """
+    운송장 아이템을 분류하는 엔드포인트입니다. 운송장 데이터를 받아서 분류 작업을 수행하고, 그 결과를 반환합니다.
+    Args:
+        item (WaybillItem): 분류할 운송장 아이템.
+    Returns:
+        WaybillItem: 분류된 운송장 아이템.
+    """
     return item
 
 @event_router.post("/analyze")
 async def analyze_event(item: EventItem):
+    """
+    이벤트 아이템을 분석하는 엔드포인트입니다. 이벤트 데이터를 받아서 분석 작업을 수행하고, 그 결과를 반환합니다.
+    Args:
+        item (EventItem): 분석할 이벤트 아이템.
+    Returns:
+        EventItem: 분석된 이벤트 아이템.
+    """ 
     return item
 
 @market_router.post("/generate")
 async def generate_market_report(request: MarketReportRequest):
+    """
+    시장 보고서를 생성하는 엔드포인트입니다. 시장 보고서 생성에 필요한 데이터를 받아서 보고서를 생성하고, 그 결과를 반환합니다.
+    Args:
+        request (MarketReportRequest): 시장 보고서 생성 요청 데이터.
+    Returns:
+        MarketReportRequest: 생성된 시장 보고서 데이터.
+    """
     return request
 
 @chatbot_router.post("/interact")
 async def interact_with_chatbot(message: ChatbotMessage):
+    """
+    챗봇과의 기본 상호작용을 처리하는 엔드포인트입니다. 사용자의 메시지를 받아 챗봇 모델로부터 응답을 생성하고 반환합니다.
+    Args:
+        message (ChatbotMessage): 사용자로부터의 챗봇 메시지.
+    Returns:
+        dict: 챗봇의 응답 메시지.
+    """ 
     target_model = models.ChatModelInvoker()
     question = message.message
     answer = target_model.generate_response(question=question)
@@ -67,6 +97,13 @@ async def interact_with_chatbot(message: ChatbotMessage):
 
 @chatbot_router.post("/interact/gpt3")
 async def interact_with_chatbot(message: ChatbotMessage):
+    """
+    GPT-3 모델을 사용하여 챗봇과 상호작용하는 엔드포인트입니다. 사용자의 메시지를 받아 GPT-3 모델로부터 응답을 생성하고 반환합니다.
+    Args:
+        message (ChatbotMessage): 사용자로부터의 챗봇 메시지.
+    Returns:
+        dict: 챗봇의 응답 메시지.
+    """    
     target_model = models.ChatGPTAPIResponder()
     question = message.message
     answer = target_model.get_response(question=question)
@@ -74,6 +111,13 @@ async def interact_with_chatbot(message: ChatbotMessage):
 
 @chatbot_router.post("/interact/phi")
 async def interact_with_chatbot(message: ChatbotMessage):
+    """
+    phi 모델을 사용하여 챗봇과 상호작용하는 엔드포인트입니다. 사용자의 메시지를 받아 phi 모델로부터 응답을 생성하고 반환합니다.
+    Args:
+        message (ChatbotMessage): 사용자로부터의 챗봇 메시지.
+    Returns:
+        dict: 챗봇의 응답 메시지.
+    """
     target_model = models.phi_instance
     question = message.message
     answer = target_model.get_response(question=question)
--- app/module/models.py
+++ app/module/models.py
@@ -6,26 +6,49 @@ import csv
 import openai
 from dotenv import load_dotenv
 import os
+import numpy as np
 from datetime import datetime
 from llama_cpp import Llama
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-class ChatModelInvoker:
+class BedrockInvoker:
+    """
+    AWS Bedrock Runtime을 사용하여 모델을 호출하기 위한 기본 클래스입니다.
+    AWS의 Bedrock 서비스와 연동하여 모델을 호출하는 데 필요한 공통 속성과 메서드를 정의합니다.
+    """
     def __init__(self):
         self.region_name = 'us-east-1'
         self.runtime_client = boto3.client(
                 service_name='bedrock-runtime',
                 region_name=self.region_name,
-            )
-        self.model_name = 'anthropic.claude-instant-v1'
+            ) 
         self.content_type = 'application/json'
         self.accept = 'application/json'
+
+class ChatModelInvoker(BedrockInvoker):
+    """
+    Bedrock 기반의 챗봇 모델을 호출하여 대화 응답을 생성하는 클래스입니다.
+    사용자의 질문에 대한 모델의 응답을 생성하는 메서드를 포함합니다.
+    """    
+    def __init__(self):
+        super().__init__()  
+        self.model_name = 'anthropic.claude-instant-v1'
         self.prompt_intro = "\n\nHuman:"
         self.response_intro = "\n\nAssistant:"
 
     def generate_response(self, question, max_tokens=500, temperature=0, top_p=0.9):
+        """
+        질문에 대한 모델의 응답을 생성합니다.
+        Args:
+            question (str): 사용자로부터 받은 질문.
+            max_tokens (int): 생성할 최대 토큰 수.
+            temperature (float): 샘플링 온도.
+            top_p (float): 토큰 샘플링에 사용되는 확률의 상위 퍼센트.
+        Returns:
+            dict: 모델이 생성한 응답을 담은 딕셔너리.
+        """
         prompt = self.prompt_intro + question + self.response_intro
         request_body = json.dumps({
             "prompt": prompt,
@@ -43,11 +66,115 @@ class ChatModelInvoker:
 
         return response_body.get('completion')
 
+class EmbeddingInvoker(BedrockInvoker):
+    """
+    텍스트 임베딩을 생성하기 위한 클래스입니다.
+    다양한 임베딩 모델을 호출하여 텍스트 데이터에 대한 벡터 임베딩을 생성하는 메서드를 포함합니다.
+    """    
+    def __init__(self):
+        super().__init__()
+
+    def coher_embedding(self, prompt):
+        """
+        Cohere 모델을 사용하여 주어진 텍스트 프롬프트에 대한 임베딩을 생성합니다.
+        Args:
+            prompt (str): 임베딩을 생성할 텍스트 프롬프트.
+        Returns:
+            numpy.ndarray: 생성된 임베딩 벡터.
+        """        
+        cohereModelId = 'cohere.embed-multilingual-v3'
+
+        # Ensure that the prompt is always a list of strings
+        if not isinstance(prompt, list):
+            prompt = [prompt]  # Convert a single string prompt to a list
+
+        coherePayload = json.dumps({
+            'texts': prompt,
+            'input_type': 'search_document',
+            'truncate': 'NONE'
+        })
+
+        print("\nInvoking Cohere Embed...")
+        response = self.runtime_client.invoke_model(
+            body=coherePayload, 
+            modelId=cohereModelId, 
+            accept=self.accept, 
+            contentType=self.content_type
+        )
+
+        body = response.get('body').read().decode('utf-8')
+        response_body = json.loads(body)
+        return_val = np.array(response_body['embeddings'])[0]
+        return return_val
+        
+    def generate_embeddings(self, body):
+        """
+        Amazon Titan Embeddings G1 모델을 사용하여 주어진 텍스트에 대한 임베딩을 생성합니다.
+        Args:
+            body (str): 요청 본문.
+        Returns:
+            dict: 생성된 임베딩을 포함하는 응답 본문.
+        """
+
+
+        model_id = "amazon.titan-embed-text-v1"
+
+        response = self.runtime_client.invoke_model(
+            body=body, modelId=model_id, accept=self.accept, contentType=self.content_type
+        )
+
+        response_body = json.loads(response.get('body').read())
+
+        return response_body
+
+    def amazon_g1_embedding(self,prompt):
+        """
+        주어진 텍스트 프롬프트에 대한 Amazon G1 임베딩을 생성합니다.
+        Args:
+            prompt (str): 임베딩을 생성할 텍스트 프롬프트.
+        Returns:
+            numpy.ndarray: 생성된 임베딩 벡터.
+        """        
+        body = json.dumps({
+        "inputText": prompt,
+    })
+        response = self.generate_embeddings(body)
+        embedding_res = response['embedding']
+        return_val = np.array(embedding_res)
+        return return_val
+
+    def cosine_similarity(self,A,B):
+        """
+        두 벡터 간의 코사인 유사도를 계산합니다.
+        Args:
+            A (numpy.ndarray): 첫 번째 벡터.
+            B (numpy.ndarray): 두 번째 벡터.
+        Returns:
+            float: 두 벡터 간의 코사인 유사도.
+        """        
+        return np.dot(A, B) / (np.linalg.norm(A) * np.linalg.norm(B))
+
+
+
 class ChatGPTAPIResponder:
+    """
+    OpenAI의 ChatGPT API를 사용하여 텍스트 응답을 생성하는 클래스입니다.
+    사용자의 프롬프트에 대한 응답을 생성하는 메서드를 포함합니다.
+    """    
     def __init__(self):
         self.api_key = openai.api_key
 
     def get_response(self, prompt, engine="gpt-3.5-turbo", max_tokens=250, temperature=0.5):
+        """
+        주어진 프롬프트에 대한 응답을 생성합니다.
+        Args:
+            prompt (str): 사용자로부터 받은 프롬프트.
+            engine (str): 사용할 OpenAI 엔진.
+            max_tokens (int): 생성할 최대 토큰 수.
+            temperature (float): 샘플링 온도.
+        Returns:
+            list: 모델이 생성한 응답 메시지를 포함하는 리스트.
+        """        
         response = openai.ChatCompletion.create(
             model=engine,
             messages=[{"role": "system", "content": "Analyze the following review and provide a structured analysis."},
@@ -60,6 +187,10 @@ class ChatGPTAPIResponder:
 
 
 class phi:
+    """
+    phi2 모델을 사용하여 텍스트 응답을 생성하는 클래스입니다.
+    사용자의 질문에 대한 응답을 생성하는 메서드를 포함합니다.
+    """    
     def __init__(self) -> None:
         self.llm = Llama(
         model_path="model/phi-2.Q6_K.gguf",
@@ -67,6 +198,13 @@ class phi:
         pass
 
     def get_response(self, question):
+        """
+        주어진 질문에 대한 응답을 생성합니다.
+        Args:
+            question (str): 사용자로부터 받은 질문.
+        Returns:
+            str: 모델이 생성한 응답.
+        """
         answer = self.llm.create_chat_completion(
         max_tokens=32,
         stop=["###", "\n\n"],
@@ -80,15 +218,8 @@ class phi:
               "content": question
           }
         ])
-        # print(answer)
-        # return answer
         for choice in answer['choices']:
             message = choice['message']
             if message['role'] == 'assistant':
                 return message['content']
 
-phi_instance = phi()
-if __name__== '__main__':
-    test = phi()
-    q= "Describe the delicous apple."
-    print(test.get_response(q))
